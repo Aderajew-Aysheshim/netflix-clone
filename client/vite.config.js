3e149f1e39d16f316ff1b1888f7f59925aefd87d
eteh64 <aysheshimaderajew600@gmail.com>
2025-12-23 10:23:52 +0300

    fix import react

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,6 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    // Define base directory for assets and public files
-    base: '/',
-    resolve: {
-        alias: {
-            // Resolve paths properly for imports
-            '@': '/src',
-        },
-    },
-    build: {
-        // Ensure that assets like images are handled correctly
-        assetsDir: 'assets',
-    },
-    server: {
-        // Enable to expose local server externally (useful if testing on other devices)
-        host: '0.0.0.0',
-        port: 5173,
-        hmr: true, // Hot Module Replacement for real-time updates
-    },
+    plugins: [react()],
 });
